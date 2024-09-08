3e435db475b6f2e5fb58fc867ce6806f5e0947f6
Kirill Bannyi <kbannyi@custis.ru>
2024-09-08 15:34:47 +0300

    feat: цветные логи

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kbannyi/gophermart
 
 go 1.22.4
+
+require github.com/lmittmann/tint v1.0.5
--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -3,13 +3,20 @@ package logger
 import (
 	"log/slog"
 	"os"
+	"time"
+
+	"github.com/lmittmann/tint"
 )
 
 var Log *slog.Logger = slog.Default()
 
 func Initialize() {
-	var programLevel = new(slog.LevelVar)
-	programLevel.Set(slog.LevelDebug)
-	Log = slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: programLevel}))
+	// zero-dependency handler that writes tinted (colorized) logs
+	handler := tint.NewHandler(os.Stderr, &tint.Options{
+		Level:      slog.LevelDebug,
+		TimeFormat: time.Kitchen,
+	})
+
+	Log = slog.New(handler)
 	slog.SetDefault(Log)
 }
